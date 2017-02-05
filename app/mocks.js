d51e3b81faddee33bbc4da06541deb42b337a669
bacondrake <frank.pavey@gmail.com>
2017-02-05 21:57:08 +1100

    basic styles, unscaled images

--- app/mocks.js
+++ app/mocks.js
@@ -4,28 +4,36 @@ exports.CARPARTS = [{
         "name": "Super Tires",
         "description": "These tires are the very best",
         "inStock": 5,
-        "price": 4.99
+        "price": 4.99,
+        "image": "/images/tire.jpg",
+        "featured": false
     },
     {
         "id": 2,
         "name": "Reinforced Shocks",
         "description": "Shocks made from Kryptonite",
         "inStock": 4,
-        "price": 9.99
+        "price": 9.99,
+        "image": "/images/shocks.jpg",
+        "featured": false
     },
     {
         "id": 3,
         "name": "NOS button",
         "description": "Gives you the boost you need to win respect on the streets",
         "inStock": 0,
-        "price": 16.99
+        "price": 16.99,
+        "image": "/images/nos.jpg",
+        "featured": true
     },
     {
         "id": 4,
         "name": "Padded seats",
         "description": "Super soft seats for a smooth ride",
         "inStock": 1,
-        "price": 8.99
+        "price": 8.99,
+        "image": "/images/seats.jpg",
+        "featured": false
     }
 ];
 //# sourceMappingURL=mocks.js.map
